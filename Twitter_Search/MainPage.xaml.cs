03a35643fd902aaafb6e0845d0385bdf7a3676f8
Samarth <shahsamarth1991@gmail.com>
2013-12-23 22:36:14 +0530

    Development Date 15-08-12

---
+++ Twitter_Search/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Xml.Linq;
+
+namespace Twitter_Search
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnLookupUser_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient twitter = new WebClient();
+            twitter.DownloadStringCompleted += new DownloadStringCompletedEventHandler(twitter_DownloadStringCompleted);
+            twitter.DownloadStringAsync(new Uri("http://search.twitter.com/search.atom?q=%40twitterapi"));
+        }
+
+
+        void twitter_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                return;
+
+            XElement xmlTweets = XElement.Parse(e.Result);
+
+            lstTwitter.ItemsSource = from tweet in xmlTweets.Descendants("status")
+                                     select new TwitterItem
+                                     {
+                                         ImageSource = tweet.Element("user").Element("profile_image_url").Value,
+                                         Message = tweet.Element("text").Value
+                                     };
+        }
+    }
+}
